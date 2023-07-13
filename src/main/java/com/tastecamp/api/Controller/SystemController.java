8c5f000731b45e9afef49538f8a1acc93458c814
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-07-12 22:31:12 -0300

    feat: cors

--- src/main/java/com/tastecamp/api/Controller/SystemController.java
+++ src/main/java/com/tastecamp/api/Controller/SystemController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -23,6 +25,7 @@ import com.tastecamp.api.model.Candidate;
 
 @RestController
 @RequestMapping("/api/v1/hiring")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class SystemController {
 
     @Autowired
